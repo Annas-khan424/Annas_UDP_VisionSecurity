b78156df585f6647888f25ddadcf9c6a15139b7b
Annas-khan424 <annas.md.khan424@gmail.com>
2023-08-14 22:38:38 +0100

    setup flask app and connect to database

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, send_from_directory, session, jsonify
+from mtcnn import MTCNN
+from flask_restful import Api, Resource
+import cv2
+from flaskext.mysql import MySQL
+import os
+import qrcode
+import numpy as np
+from PIL import Image
+import tempfile
+import shutil
+import numpy as np
+import face_recognition
+from pyzbar import pyzbar
+
+
+app = Flask(__name__)
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'vision_sec'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql = MySQL(app)
+
+app.secret_key = 'secret_key_here'
+app.config['UPLOAD_FOLDER'] = 'uploads'
+
+# Function to save student photo and return the path
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
