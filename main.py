00e4da2baeaedcaac938d9dc56038d7efab1efb4
Annas-khan424 <annas.md.khan424@gmail.com>
2023-08-24 14:15:30 +0100

    prints statements

--- main.py
+++ main.py
@@ -41,3 +41,41 @@ try:
                 response = requests.post(url, files=files)
                 
             speak("Photo sent. Server response received.")
+            # Print the response
+            print("Response:", response.text)
+            
+            # Process the server response
+            try:
+                response_json = response.json()
+                if "qr_code_data" in response_json:
+                    qr_code_data = response_json["qr_code_data"]
+                    if len(qr_code_data) > 0:
+                        user_info = qr_code_data[0]
+                        if "Name:" in user_info and "ID:" in user_info:
+                            user_name = user_info.split("Name:")[1].split(",")[0].strip()
+                            user_id = user_info.split("ID:")[1].split(",")[0].strip()
+                            speak("Welcome, " + user_name + ". Your ID is " + user_id)
+                        else:
+                            print("Invalid QR code data format")
+                            speak("Invalid QR code data format")
+                    else:
+                        print("No QR code data found")
+                        speak("No QR code data found")
+                elif "result" in response_json:
+                    face_match_result = response_json["result"]
+                    if "Face matches" in face_match_result:
+                        user_name = face_match_result.split("with")[1].strip()
+                        speak("Welcome, " + user_name[:-10])  # Remove "_photo.jpg" part
+                    else:
+                        print("Invalid photo: No face or multiple faces detected")
+                        speak("Invalid photo: No face or multiple faces detected")
+                elif "error" in response_json:
+                    error_message = response_json["error"]
+                    print(error_message)
+                    speak(error_message)
+                else:
+                    print("Unexpected server response:", response.text)
+            except Exception as e:
+                print("Error processing server response:", str(e))
+                speak("Error processing server response")
+            
