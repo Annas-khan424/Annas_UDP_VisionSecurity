4c044c35257bed83a3f3981e654c6b2d7581686e
Annas-khan424 <annas.md.khan424@gmail.com>
2023-08-24 14:12:32 +0100

    motion sensor code

--- main.py
+++ main.py
@@ -18,5 +18,26 @@ def speak(text):
     engine.runAndWait()
 
 # Raspberry Pi IP and URL
-raspberry_pi_ip = "192.168.1.2"
+raspberry_pi_ip = "172.20.10.2"
 url = f"http://{raspberry_pi_ip}:5000/process_photo"
+
+try:
+    speak("Motion detection is armed. Waiting for motion...")
+    
+    while True:
+        if GPIO.input(PIR_PIN):
+            speak("Motion detected!")
+            
+            # Capture a photo with the highest quality using fswebcam
+            photo_filename = "captured_photo.jpg"
+            subprocess.run(["fswebcam", "-r", "1920x1080", "--no-banner", photo_filename])
+            speak("Photo captured.")
+            
+            # Prepare the data payload
+            with open(photo_filename, "rb") as photo_file:
+                files = {'file': (photo_filename, photo_file, 'image/jpeg')}
+                
+                speak("Sending the photo to the server.")
+                response = requests.post(url, files=files)
+                
+            speak("Photo sent. Server response received.")
